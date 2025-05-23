4a2baba5936851c8ce8d76811457471cd7372827
darafadel <darafadelprogramacao@gmail.com>
2025-05-23 10:51:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,3 +5,7 @@ st.write("""
 # My First App
 Hello *world!*
 """)
+st.text_input("Digite seu nome completo:")
+st.text_input("Digite sua idade:")
+st.text_input("Digite seu email:")
+st.text_input("Digite seu CPF:)
